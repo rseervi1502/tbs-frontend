4cb246a01d87a722f6ceb1a5834a6613baa286f4
rseervi1502 <125062729+rseervi1502@users.noreply.github.com>
2023-03-07 19:39:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={log} className="App-logo" alt="logo" />
-        <p>
-          Learn <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Welcome to Taggka business solution </p>
+      <p>Platform is under in devolopment </p>
     </div>
   );
 }
