b3a5cba7ffb4d7fcc9c4af0afafdac2b4f513798
rseervi1502 <125062729+rseervi1502@users.noreply.github.com>
2023-03-07 19:36:52 +0530

    4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import log from './Resource/log.jpg';
+import log from './Log.jpg';
 import './App.css';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
           Learn <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link" 
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
