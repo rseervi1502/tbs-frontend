6ea640cf57094c453c6f354eb7fff583e917925f
rseervi1502 <125062729+rseervi1502@users.noreply.github.com>
2023-03-07 19:25:30 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import log from './Resource';
+import log from './Resource/log.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={log} className="App-logo" alt="logo" />
         <p>
-          Learn react <code>src/App.js</code> and save to reload.
+          Learn <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
