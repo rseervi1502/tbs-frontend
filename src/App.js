053a9ae923141c2179e191b9fd5c5f3378181fe5
rseervi1502 <125062729+rseervi1502@users.noreply.github.com>
2023-03-11 23:09:38 +0530

    g

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You would typically make an API call here to verify the username and password
+    // and set the loggedIn state accordingly.
+    setLoggedIn(true);
+  };
+
+  if (loggedIn) {
+    return <h1>Welcome {username}!</h1>;
+  }
+
   return (
-    <div>
-      <p>Welcome to Taggka business solution </p>
-      <p>Platform is under in devolopment </p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <button type="submit">Login</button>
+    </form>
   );
-}
+};
 
-export default App;
+export default Login;
